23ef37e8e3262a362a0a61bd33000d68b6580eb0
hydrielax <alexidelage@gmail.com>
2021-12-02 10:53:55 +0100

    Update package name

--- medev_tp3/src/main/java/org/centrale/medev_tp3/Image.java
+++ medev_tp3/src/main/java/org/centrale/medev_tp3/Image.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.centralenantes.medev;
+package org.centrale.medev_tp3;
 
 /**
  *

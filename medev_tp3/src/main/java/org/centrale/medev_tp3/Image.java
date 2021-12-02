95d40b70271c6750635fddb577d7a8985c10e1d8
Defossez <floran.defossez@gmail.com>
2021-12-02 11:05:27 +0100

    attributs image

--- medev_tp3/src/main/java/org/centrale/medev_tp3/Image.java
+++ medev_tp3/src/main/java/org/centrale/medev_tp3/Image.java
@@ -10,6 +10,46 @@ package org.centrale.medev_tp3;
  * @author floran
  */
 public class Image {
+    private int longueur;
+    private int largeur;
+    private int echelle;
+    private int[][] pixels;
+    
+
+    public int getLongueur() {
+        return longueur;
+    }
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public int getEchelle() {
+        return echelle;
+    }
+
+    public int[][] getPixels() {
+        return pixels;
+    }
+
+    public void setLongueur(int longueur) {
+        this.longueur = longueur;
+    }
+
+    public void setLargeur(int largeur) {
+        this.largeur = largeur;
+    }
+
+    public void setEchelle(int echelle) {
+        this.echelle = echelle;
+    }
+
+    public void setPixels(int[][] pixels) {
+        this.pixels = pixels;
+    }
+    
+    
+    
     
     
 }
